0f079d42051506248ca4b25ee1b52a48c1c49ee6
Cliff Dean <cliff.dean@eannovate.com>
2018-12-06 13:08:43 +0700

    adjust return api category with status, message, data, error

--- api/controllers/categoryController.js
+++ api/controllers/categoryController.js
@@ -1,12 +1,45 @@
 "use strict";
+var functions = require("../../helpers/functions");
 var mongoose = require("mongoose"), Category = mongoose.model("categories");
 
 exports.get_all = function(req, res) {
 	Category.find({}, function(err, data) {
 		if(err){
-			res.send(err);
+			functions.ArrayResponse(res, 404, "Error", err);
 		}else{
-			res.json(data);
+			if(functions.checkArray(data)){
+				functions.ArrayResponse(res, 200, "Data Exist", data);
+			}else{
+				functions.BaseResponse(res, 400, "No Data");
+			}
+		}
+	});
+};
+
+exports.get_all_by_type = function(req, res) {
+	Category.find({type: req.params.type}, function(err, data) {
+		if(err){
+			functions.ArrayResponse(res, 404, "Error", err);
+		}else{
+			if(functions.checkArray(data)){
+				functions.ArrayResponse(res, 200, "Data Exist", data);
+			}else{
+				functions.BaseResponse(res, 400, "No Data");
+			}
+		}
+	});
+};
+
+exports.get_detail = function(req, res) {
+	Category.findById(req.params.id, function(err, data) {
+		if(err){
+			functions.ArrayResponse(res, 404, "Error", err);
+		}else{
+			if(functions.checkUndefined(data)){
+				functions.ArrayResponse(res, 200, "Data Exist", data);
+			}else{
+				functions.BaseResponse(res, 400, "No Data");
+			}
 		}
 	});
 };
@@ -15,19 +48,13 @@ exports.insert_data = function(req, res) {
 	var param = new Category(req.body);
 	param.save(function(err, data) {
 		if(err){
-			res.send(err);
+			functions.ArrayResponse(res, 404, "Error", err);
 		}else{
-			res.json(data);
-		}
-	});
-};
-
-exports.get_detail = function(req, res) {
-	Category.findById(req.params.id, function(err, data) {
-		if(err){
-			res.send(err);
-		}else{
-			res.json(data);
+			if(functions.checkUndefined(data)){
+				functions.ArrayResponse(res, 200, "Success", data);
+			}else{
+				functions.BaseResponse(res, 400, "Failed");
+			}
 		}
 	});
 };
@@ -35,29 +62,27 @@ exports.get_detail = function(req, res) {
 exports.update_data = function(req, res) {
 	Category.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, data) {
 		if(err){
-			res.send(err);
+			functions.ArrayResponse(res, 404, "Error", err);
 		}else{
-			res.json(data);
+			if(functions.checkUndefined(data)){
+				functions.ArrayResponse(res, 200, "Success", data);
+			}else{
+				functions.BaseResponse(res, 400, "Failed");
+			}
 		}
 	});
 };
 
 exports.delete_data = function(req, res) {
-	Category.remove({_id: req.params.id}, function(err, data) {
+	Category.deleteOne({_id: req.params.id}, function(err, data) {
 		if(err){
-			res.send(err);
+			functions.ArrayResponse(res, 404, "Error", err);
 		}else{
-			res.json({message: "Category successfully deleted"});
-		}
-	});
-};
-
-exports.get_all_by_type = function(req, res) {
-	Category.find({type: req.params.type}, function(err, data) {
-		if(err){
-			res.send(err);
-		}else{
-			res.json(data);
+			if(data.n >= 1){
+				functions.BaseResponse(res, 200, "Success");
+			}else{
+				functions.BaseResponse(res, 400, "Failed");
+			}
 		}
 	});
 };
---
+++ helpers/functions.js
@@ -0,0 +1,15 @@
+module.exports.checkArray = function(data) {
+	return Array.isArray(data) && data.length ? true : false;
+}
+
+module.exports.checkUndefined = function(data) {
+	return typeof data !== "undefined" && data !== null ? true : false;
+}
+
+module.exports.BaseResponse = function(res, status, message) {
+	return res.json({status: parseInt(status), message: message.toString()});
+}
+
+module.exports.ArrayResponse = function(res, status, message, data = array()) {
+	return res.json({status: parseInt(status), message: message.toString(), data: data});
+}
